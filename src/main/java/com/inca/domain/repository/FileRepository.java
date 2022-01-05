66b92f9ce68221261bfb538f5129f62276bfe6d7
padmalcom <j.freiknecht@googlemail.com>
2022-01-05 16:41:14 +0100

    fixed repo annotation

--- src/main/java/com/inca/domain/repository/FileRepository.java
+++ src/main/java/com/inca/domain/repository/FileRepository.java
@@ -3,11 +3,11 @@ package com.inca.domain.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 
 import com.inca.domain.model.FileInformation;
 
-@Transactional
+@Repository
 public interface FileRepository extends CrudRepository<FileInformation, Long> {
 
 	public Optional<FileInformation> findByMd5(String md5);
