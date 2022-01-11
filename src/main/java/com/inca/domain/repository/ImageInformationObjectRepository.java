4f819525dac0e20db3da8c4f7152f2a42adadb8d
padmalcom <j.freiknecht@googlemail.com>
2022-01-11 16:43:19 +0100

    Removed spring boot helper

--- src/main/java/com/inca/domain/repository/ImageInformationObjectRepository.java
+++ src/main/java/com/inca/domain/repository/ImageInformationObjectRepository.java
@@ -3,13 +3,13 @@ package com.inca.domain.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.inca.domain.model.ImageInformationObject;
-import com.nooul.apihelpers.springbootrest.repositories.BaseRepository;
 
 @Repository
-public interface ImageInformationObjectRepository extends BaseRepository<ImageInformationObject, Long> {
+public interface ImageInformationObjectRepository extends CrudRepository<ImageInformationObject, Long> {
 	public Page<ImageInformationObject> findAll(Pageable pageable);
 	public Page<ImageInformationObject> findAll(Specification<ImageInformationObject> spec, Pageable pageable);
 }
