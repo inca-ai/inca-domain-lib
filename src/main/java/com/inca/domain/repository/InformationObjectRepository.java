a44a4a7a3bd954adbc4287d0ba2117c1efe55d7c
padmalcom <j.freiknecht@googlemail.com>
2022-01-11 20:40:16 +0100

    readded repos

--- src/main/java/com/inca/domain/repository/InformationObjectRepository.java
+++ src/main/java/com/inca/domain/repository/InformationObjectRepository.java
@@ -3,13 +3,13 @@ package com.inca.domain.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.inca.domain.model.InformationObject;
+import com.nooul.apihelpers.springbootrest.repositories.BaseRepository;
 
 @Repository
-public interface InformationObjectRepository extends CrudRepository<InformationObject, Long> {
+public interface InformationObjectRepository extends BaseRepository<InformationObject, Long> {
 	public Page<InformationObject> findAll(Pageable pageable);
 	public Page<InformationObject> findAll(Specification<InformationObject> spec, Pageable pageable);
 }
--- src/test/java/com/inca/domain/IncaDomainLibApplicationTests.java
+++ src/test/java/com/inca/domain/IncaDomainLibApplicationTests.java
@@ -1,57 +1,19 @@
 package com.inca.domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.inca.domain.model.ImageInformationObject;
-import com.inca.domain.model.InformationObject;
-import com.inca.domain.repository.ImageInformationObjectRepository;
-import com.inca.domain.repository.InformationObjectRepository;
-
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-//@DataJpaTest
-//@TestPropertySource("classpath:application-test.properties")
 @ActiveProfiles("test")
 class IncaDomainLibApplicationTests {
 
-	@Autowired
-	private InformationObjectRepository ior;
-	
-	@Autowired
-	private ImageInformationObjectRepository iior;
 
 	@Test
 	void contextLoads() {
-		ImageInformationObject iio = new ImageInformationObject();
-		iio.setName("testimage");
-		iio = iior.save(iio);
-		
-		
-		InformationObject io = new InformationObject();
-		io.setName("testname");
-		io = ior.save(io);
-		
-		System.out.println("Image id: " + iio.getId());
-		System.out.println("Object id: " + iio.getId());
-		
-		Iterable<InformationObject> all = ior.findAll();
-		for(InformationObject a : all) {
-			System.out.println(a);
-		}
-		
-		Iterable<ImageInformationObject> allImages = iior.findAll();
-		for(ImageInformationObject ia : allImages) {
-			System.out.println(ia);
-		}
-	
-		assertEquals(ior.count(), 2);
 	}
 
 }
