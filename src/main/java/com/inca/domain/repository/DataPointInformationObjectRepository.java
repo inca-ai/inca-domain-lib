a56fe5779f4c6f55e4d0d8fdc6c17b7dcedef1b6
padmalcom <j.freiknecht@googlemail.com>
2022-01-13 13:31:47 +0100

    Added DataPoint Repo

---
+++ src/main/java/com/inca/domain/repository/DataPointInformationObjectRepository.java
@@ -0,0 +1,15 @@
+package com.inca.domain.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Repository;
+
+import com.inca.domain.model.DataPointInformationObject;
+import com.nooul.apihelpers.springbootrest.repositories.BaseRepository;
+
+@Repository
+public interface DataPointInformationObjectRepository extends BaseRepository<DataPointInformationObject, Long> {
+	public Page<DataPointInformationObject> findAll(Pageable pageable);
+	public Page<DataPointInformationObject> findAll(Specification<DataPointInformationObject> spec, Pageable pageable);
+}
