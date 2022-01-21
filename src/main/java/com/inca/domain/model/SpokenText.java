f9cba181ec59ed0c41bfe92bd5b6b29c92d49449
padmalcom <j.freiknecht@googlemail.com>
2022-01-21 14:01:04 +0100

    embeddable to table

--- src/main/java/com/inca/domain/model/SpokenText.java
+++ src/main/java/com/inca/domain/model/SpokenText.java
@@ -4,11 +4,12 @@ import java.util.Locale;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
-import javax.persistence.Embeddable;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,7 +18,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Embeddable
+@Entity
+@Table(schema = "inca")
 @ToString
 @Getter
 @Setter
